aed712fe107e7c85bd640025bf892fb1ce96b8d6
doibaa42-netizen <doibaa42@gmail.com>
2026-08-12 15:51:47 +0300

    Fix component import paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 ""use client";
   
-import BookingForm from "@/app/components/BookingForm";
-import Navbar from "@/app/components/Navbar";
+import BookingForm from 
+"@/app/components/BookingForm";
+import Navbar from 
+"@/app/components/Navbar";
 type Service = {
   name: string;
   image: string;
